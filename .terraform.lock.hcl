b1b66c268010c310560e4a197f18b2e0e2ad217a
mdawy <mohamed.dawy@hotmail.com>
2023-05-02 15:12:08 +0300

    Test

---
+++ .terraform.lock.hcl
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7eafade1a1381d5e44c01180b5b1f20e35c6e946bfc30e9be99dfd2266dbc116
+size 1185
--- providers.tf
+++ providers.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:8c363a89664adfe0bc683dc8ce9bd9d6893d3a678de839f6e305987cea2d1f9a
+size 161
